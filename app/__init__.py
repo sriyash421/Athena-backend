8eb487f29f3bba1c1dc92c908cdfed59ebbe4f48
Sry-421 <poddarsriyash@gmail.com>
2020-05-18 23:13:10 +0530

    gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,13 @@ from flask_cors import CORS
 from flask_admin import Admin
 from elasticsearch import Elasticsearch
 from flask_socketio import SocketIO
-import os
+
 
 app = Flask(__name__)
 cors = CORS(app)
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URL")
-app.config["ELASTICSEARCH_URL"]= os.environ.get("ELASTIC_SEARCH")
+app.config["SECRET_KEY"] = 'f9bf78b9a18ce6d46a0cd2b0b86df9da'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
+app.config["ELASTICSEARCH_URL"]= "http://localhost:9200"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
